c1ecf1bdb73107b0256c10560c39f99283c87a51
GaMa <gama.ariesto03@gmail.com>
2022-07-12 16:59:47 +0700

    Add files via upload

---
+++ ResponsiPenjualan/Penjualan.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal
+
+        public string nota {get; set;}
+        public string tanggal {get; set;}
+        public string customer {get; set;}
+        public string jenis {get; set;}
+        public int total {get; set;}
+    }
+}
